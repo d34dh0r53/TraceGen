d2369b02a5c71391e5e184c3fb59e3f8c3f8858f
d34dh0r53 <david.wilde@rackspace.com>
2014-08-12 11:32:32 -0500

    Added output file handling

--- tracegen.py
+++ tracegen.py
@@ -6,20 +6,19 @@ import uuid
 import sys
 
 @click.command()
-@click.option('--file', default='stdout', help='Name of output file for multiline trace output')
+@click.option('--outfile', default='sys.stdout', help='Name of output file for multiline trace output')
 @click.option('--count', default=1, help='Number of indented lines')
 @click.option('--datestamp', default=datetime.datetime.now().isoformat(' '), help="Datestamp")
 @click.option('--pid', default=65535, help='Pid of logging process')
 @click.option('--level', default="TRACE", help='Logging level')
 @click.option('--logsource', default='tracegen', help='Name of the process doing the logging')
 @click.option('--message', default='TESTING TRACE', help='Message to inject on first line of multiline')
-def do_logging(file, count, logsource, datestamp, level, message, pid):
+def do_logging(outfile, count, logsource, datestamp, level, message, pid):
     my_tag = str(uuid.uuid4())
     odd_spaces = '  '
     even_spaces = '    '
 
-    print "{} {} {} {} Traceback (most recent call last): {} {}".format(datestamp, pid, level, logsource, message, my_tag)
-
+    do_output(outfile, "{} {} {} {} Traceback (most recent call last): {} {}\n".format(datestamp, pid, level, logsource, message, my_tag))
     for i in xrange(count):
         line_head = "{} {} {} {}".format(datestamp, pid, level, logsource)
         line_message = "Line {} - {}".format(i, my_tag)
@@ -29,10 +28,17 @@ def do_logging(file, count, logsource, datestamp, level, message, pid):
             line_message = even_spaces + line_message
         else:
             line_message = odd_spaces + line_message
-        print "{} {}".format(line_head, line_message)
+        do_output(outfile,"{} {}\n".format(line_head, line_message))
 
-    print "{} {} {} {}".format(datestamp, pid, level, logsource, message)
+    do_output(outfile,"{} {} {} {}\n".format(datestamp, pid, level, logsource, message))
     sys.stderr.write(my_tag + '\n')
+
+def do_output(fh, line):
+    if fh == 'sys.stdout':
+        sys.stdout.write(line)
+    else:
+        out_fh = open(fh,'a+',0)
+        out_fh.write(line)
 
 if __name__ == '__main__':
     do_logging()
